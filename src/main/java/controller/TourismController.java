ab62da4a2da643ca779087213be54ca8b0b04aca
yeonkyung ryu <celina.16161616@gmail.com>
2023-09-13 19:21:57 +0900

    api json 성공함

--- src/main/java/controller/TourismController.java
+++ src/main/java/controller/TourismController.java
@@ -1,9 +1,49 @@
 package controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//Restful API 처리하는 컨트롤러 클래스 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+//Restful API 처리하는 컨트롤러 클래스
 @RestController
-public class RestTestController {
+public class TourismController {
+
+    @GetMapping("/apitest")
+    public String callApi() {
+
+        StringBuilder result = new StringBuilder();
+        try {
+            String apiUrl = "https://apis.data.go.kr/B551011/KorService1/searchFestival1?" +
+                    "serviceKey=bx85ZQyXZ%2BzD6Aah4J9Bz7yZ55piQrVEGw0378dm6fkhBh6mQIFwhoQTcpjUKSbZ3NVfcN%2BtdmU2RHEpNpcIzg%3D%3D" +
+                    "&numOfRows=10" +
+                    "&MobileOS=ETC" +
+                    "&MobileApp=AppTest" +
+                    "&_type=json" +
+                    "&listYN=Y" +
+                    "&arrange=A" +
+                    "&eventStartDate=20170901";
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8));
+
+            String returnLine;
+            while((returnLine= bufferedReader.readLine())!=null) {
+                result.append(returnLine).append("\n");
+            }
+            urlConnection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
 }
